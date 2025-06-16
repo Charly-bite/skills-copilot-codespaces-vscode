6b498a54223ebbdf32383f7502fa9c84bcbff1ca
Carlos Alberto Aceves Cabrera <carlos.aceves6195@alumnos.udg.mx>
2025-06-16 20:50:40 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,18 +1,5 @@
-function SkillMember(name, skill) {
-  this.name = name;
-  this.skill = skill;
-
-  this.getName = function() {
-    return this.name;
-  };
-
-  this.getSkill = function() {
-    return this.skill;
-  };
-
-  this.setSkill = function(newSkill) {
-    this.skill = newSkill;
-  };
+function SkillMember(){
+  this.name = "";
+  this.level = 0;
+  this.experience = 0;
 }
-
-  
